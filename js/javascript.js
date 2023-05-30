2f2fe84b23e88b1ba31447ab6c7ca7e139fb9c82
GutierrezVan <133538246+GutierrezVan@users.noreply.github.com>
2023-05-30 00:14:28 -0300

    Add files via upload
    ultima version

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+let cadFooter=`
+<div>
+  <label>Nuestros canales de comunicacion</label>
+                <ul class="social">
+                    <li><a href="https://www.facebook.com/nexotecnologico/"><img src="./Imagenes/favicon-facebook.ico" alt="Facebook"></a></li>
+                    <li><a href="https://twitter.com/nexotecno"><img src="./Imagenes/favicon-twitter.ico" alt="Twitter"></a></li>
+                    <li><a href="https://www.instagram.com/nexotecnologico/"><img src="./Imagenes/favicon-instagram.ico" alt=""></a></li>
+                    <li><a href="https://api.whatsapp.com/send/?phone=5491138405760"><img src="./Imagenes/favicon-whatsapp.ico" alt="Whatsapp"></a></li>
+                    <li><a href="https://www.youtube.com/@nexotecnologico"><img src="./Imagenes/favicon-youtube.ico" alt="Youtube"></a></li>
+                </ul>
+                <p> &copy; 2023 Nexo Tecnologico. All rights reserved.</p>
+  </div>
+  `
+ document.querySelector("footer").innerHTML=cadFooter
